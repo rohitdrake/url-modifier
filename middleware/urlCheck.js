785cb932466bc1156c56e762c8e0c0a85b5a62cd
rohitdrake <pirnagrasishwa@gmail.com>
2017-04-11 13:05:07 +0530

    Initital commit

---
+++ middleware/urlCheck.js
@@ -0,0 +1,54 @@
+const MongoClient = require('mongodb').MongoClient;
+
+
+let database;
+
+MongoClient.connect('mongodb://localhost:27017/users', function(err, db){
+    database=db;
+});
+
+module.exports = function(req, res, next) {
+  console.log("LOG");
+  let sent_url = req.params.id;
+  let unique_number;
+  let result = /\w{1,}\.(com|in|org)/.test(sent_url);
+  console.log(result);
+  if(!result) {
+    res.status(406).send("Invalid URL");
+    // database.close();
+    return;
+  }
+  let urlCollection = database.collection('urls');
+  let array_url_promise = urlCollection.find().toArray();
+  array_url_promise.then(function(arr){
+    if(arr.length===0){
+      unique_number=0;
+      req.urlObject={unique_number, sent_url};
+      next();
+    } else {
+
+      arr.forEach((doc)=>{
+      result = (doc.sent_url!==sent_url);
+      console.log(result);
+      if(!result){
+        console.log("Duplicate");
+        return;
+      }
+      });
+      if(result){
+        console.log(arr);
+        unique_number=arr[arr.length-1].unique_number+1;
+        req.urlObject={unique_number, sent_url};
+        console.log(req.urlObject);
+        // database.close();
+        next();
+        return;
+      }
+      res.status(406).send("Duplicate address");
+      // database.close();
+    }
+  }).catch((e)=>{
+  res.status(404).send("Unable to save");
+  // database.close();
+  });
+}
---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const urlCheck = require('./middleware/urlCheck');
+
+let env = process.env.NODE_ENV || 'development';
+let port = process.env.PORT || 3000;
+
+if(env === 'development') {
+  MongoClient.connect('mongodb://localhost:27017/users', function(err, db){
+      database=db;
+  });
+} else {
+  MongoClient.connect(process.env.MONGOLAB_URI, function(err, db){
+      database=db;
+  });
+}
+
+let database;
+
+
+
+
+let app = express();
+
+app.get('/new/:id', urlCheck,(req, res)=>{
+  let urlCollection = database.collection('urls');
+  urlCollection.insertOne(req.urlObject)
+  .then((docs)=>{
+    res.send(req.urlObject);
+    // database.close();
+  })
+  .catch((e)=>{
+    // database.close();
+    res.status(404).send("Unexpected Error")
+  });
+});
+
+app.get('/get/:id',(req, res)=>{
+    let id = Number(req.params.id);
+    console.log(typeof id);
+    let urlCollection = database.collection('urls');
+    urlCollection.findOne({unique_number: id})
+    .then((doc)=>{
+      console.log(doc);
+      if(!doc) {
+        res.status(400).send("The provided url does't exist in our database")
+        return;
+      };
+      res.writeHead(301,{Location: 'https://'+doc.sent_url});
+      res.end();
+    }).catch((e)=>{
+      res.status(404).send("Something Unexpected!!!!");
+    })
+});
+
+app.get('/get/', (req, res)=>{
+      res.status(400).send("The provided url does't exist in our database");
+});
+
+
+
+app.listen(port, ()=>{
+  console.log("Server listening at port 3000");
+});
